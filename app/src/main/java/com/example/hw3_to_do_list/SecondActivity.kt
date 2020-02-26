10352c41a224d6f85052535a37c2de569657c2e0
AdrianAlucard <wartinator@gmail.com>
2020-02-26 13:28:04 -0500

    finish second activity ui and added navigation

--- app/src/main/java/com/example/hw3_to_do_list/SecondActivity.kt
+++ app/src/main/java/com/example/hw3_to_do_list/SecondActivity.kt
@@ -2,11 +2,16 @@ package com.example.hw3_to_do_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+    }
+
+    fun goBack(view: View) {
+        finish()
     }
 }
