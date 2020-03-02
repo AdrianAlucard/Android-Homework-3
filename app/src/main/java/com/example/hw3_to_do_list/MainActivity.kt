448fadd8a05b5a4f823bba033ca137fa8f069ba2
AdrianAlucard <wartinator@gmail.com>
2020-03-01 19:50:35 -0500

    Shared pref data working

--- app/src/main/java/com/example/hw3_to_do_list/MainActivity.kt
+++ app/src/main/java/com/example/hw3_to_do_list/MainActivity.kt
@@ -1,18 +1,57 @@
 package com.example.hw3_to_do_list
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private val FILE_NAME = "TaskList"
+    private val TASK_LIST = "tasks"
+    private var taskList = mutableListOf<String>()
+    private lateinit var taskListAdapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        load()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "Restarting MainActivity")
+        super.onRestart()
+        load()
+    }
+
+    private fun load() {
+        Log.d(TAG, "Loading shared pref data")
+        val sharedPreferences = getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        val tasks = sharedPreferences.getString(TASK_LIST, "") ?: ""
+
+        if(tasks.isNotEmpty()) {
+            Log.d(TAG, "Shared preferences not empty :: Loading shared preferences")
+            val sType = object : TypeToken<MutableList<String>>() {}.type // create object of typetoken
+
+            val savedTaskList = Gson().fromJson<MutableList<String>>(tasks, sType)
+            taskList = mutableListOf()
+            taskList.addAll(savedTaskList)
+            Log.d(TAG, "Saved tasks: \n $taskList")
+            taskListAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, taskList)
+            task_list.adapter = taskListAdapter
+        } else {
+            Log.d(TAG, "No shared pref data to load")
+        }
     }
 
     fun addToDoClick(view: View) {
+        Log.d(TAG, "Starting second activity")
         val secondActivity = Intent(this, SecondActivity::class.java)
         startActivity(secondActivity)
     }
--- app/src/main/java/com/example/hw3_to_do_list/SecondActivity.kt
+++ app/src/main/java/com/example/hw3_to_do_list/SecondActivity.kt
@@ -1,17 +1,85 @@
 package com.example.hw3_to_do_list
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+    private val TAG = "SecondActivity"
+    private val FILE_NAME = "TaskList"
+    private val TASK_LIST = "tasks"
+    private lateinit var taskList:MutableList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        taskList = loadTaskList()
     }
 
     fun goBack(view: View) {
         finish()
+    }
+
+    fun saveAndGoBack(view: View) {
+        if(isUserTaskValid()) {
+                Log.d(TAG, "Save and Go Back $user_task")
+                createAndSaveTask(user_task.text.toString())
+                Log.d(TAG, "Saved Task and returning to MainActivity")
+                finish()
+        }
+    }
+
+    private fun loadTaskList(): MutableList<String> {
+        val sharedPreferences = getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        val tasks = sharedPreferences.getString(TASK_LIST, "") ?: ""
+
+        if(tasks.isNotEmpty()) {
+            Log.d(TAG, "Shared preferences not empty \n Loading shared preferences")
+            val sType = object : TypeToken<List<String>>() {}.type // create object of typetoken
+
+            val savedTaskList = Gson().fromJson<List<String>>(tasks, sType)
+            taskList = mutableListOf()
+            taskList.addAll(savedTaskList)
+            Log.d(TAG, "Saved tasks: \n $taskList")
+            return taskList
+        }
+        return mutableListOf()
+    }
+
+    private fun createAndSaveTask(task: String) {
+        val sharedPreferences = getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        taskList.add(task)
+        Log.d(TAG, "Task List after createAndSave $taskList")
+        editor.putString(TASK_LIST, Gson().toJson(taskList))
+        editor.apply()
+    }
+
+    private fun isUserTaskValid(): Boolean {
+        return isUserTextEmpty() && isDuplicateTask()
+    }
+
+    private fun isDuplicateTask(): Boolean {
+      return when(taskList.contains(user_task.text.toString())) {
+            true -> {Log.d(TAG, "user text is duplicate");
+                    Toast.makeText(this, "${user_task.text} is a duplicate", Toast.LENGTH_SHORT).show();
+                    return false}
+            false -> {true}
+        }
+    }
+
+    private fun isUserTextEmpty(): Boolean {
+        return when(user_task.text.toString().isEmpty()) {
+            true -> {Log.d(TAG, "user text is empty");
+                Toast.makeText(this, "Must enter a task in field", Toast.LENGTH_SHORT).show();
+                return false}
+            false -> {true}
+        }
     }
 }
